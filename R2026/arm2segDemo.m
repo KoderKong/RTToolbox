53ab40d706ba39f19edaa692b672bb3ad7416d6b
Dileepan Joseph <dil.joseph@ualberta.ca>
2026-07-15 16:09:29 -0400

    Update demo and license files
    Demo file updates follow the sharing of an arXiv preprint about the work. The license file has been renamed to the most common format on GitHub.

--- R2026/arm2segDemo.m
+++ R2026/arm2segDemo.m
@@ -1,45 +1,64 @@
 %%
-% mex -R2018 -Drrlu spmex1.c
-% mex -R2018 -Dbsx spmex1.c
-mex -R2018 spmex1.c
-format compact
+% mex -R2018a -Drrlu spmex1.c
+% mex -R2018a -Dbsx spmex1.c
+mex -R2018a spmex1.c
 %%
 clear
+format compact
 a = arm2seg(1/3,4/3);
 [Qv,Qu,r,Id] = qr(a,2);
-assert(nnz(r) >= 174)
+% [Qv,Qu,r,Id] = qr(a,2,'MaxIterations',Inf);
 %%
+iseq = @(a,b,n) isequal( ...
+    round(a,n),round(b,n));
 I = speye(size(Qu,2));
 assert(isequal(Id(:,:,end),I))
 assert(isequal(nnz(Id),nnz(I)))
-iseq = @(a,b,n) isequal( ...
-    round(a,n),round(b,n));
 Qu0 = Qu(:,:,end);
 assert(iseq(Qu0'*Qu0,I,3))
+% assert(iseq(Qu0'*Qu0,I,6)) % 1226 iterations
 [vs,~,k] = vsigma(Qv,Qu');
 assert(iseq(Qv*Qu'*vs,Id(k),2))
+% assert(iseq(Qv*Qu'*vs,Id(k),4)) % 1226 iterations
 %%
-% a = arm2seg(1/3,4/3);
-% r = qr(a,2); % 400 iterations
+assert(nnz(r) >= 174)
 r(abs(r) < 1e-15) = 0;
-assert(isequal(nnz(r),118))
+assert(nnz(r) == 118)
 x1Qr = roots(r(end,1,index(r)));
 x1GB = roots([68/9 -68/27 -287/81]);
 assert(iseq(x1Qr,x1GB,15))
 %%
-clear
+r = qr(a,2,'MaxIterations',0);
+assert(nnz(r) >= 48)
+r(abs(r) < 1e-15) = 0;
+assert(nnz(r) == 19)
+x1Qr = roots(r(end,1,index(r)));
+assert(iseq(x1Qr,x1GB,15))
+%%
+[Qv,Qu,~,Id] = qr(a,2,'MaxIterations',0);
+Qu0 = Qu(:,:,end);
+assert(iseq(Qu0'*Qu0,I,15))
+[vs,~,k] = vsigma(Qv,Qu');
+assert(~iseq(Qv*Qu'*vs,Id(k),0))
+%%
+[~,Qu,Qv,Id] = arm2seg(1/3,4/3);
+Qu0 = Qu(:,:,end);
+assert(~iseq(Qu0'*Qu0,I,0))
+[vs,~,k] = vsigma(Qv,Qu');
+assert(iseq(Qv*Qu'*vs,Id(k),15))
+%%
 [a,Qu,Qv,Id] = arm2seg(1/3,4/3);
 [Qu,Qv,R] = qq(Qu,Qv); % Transform
 r = Qu'*a*vsigma(Qu',a);
+assert(nnz(r) >= 31)
 r(abs(r) < 1e-16) = 0;
-assert(isequal(nnz(r),18))
+assert(nnz(r) == 18)
 %%
-I = speye(size(Qu,2));
-assert(isequal(Id(:,:,end),I))
-assert(isequal(nnz(Id),nnz(I)))
-iseq = @(a,b,n) isequal( ...
-    round(a,n),round(b,n));
 Qu0 = Qu(:,:,end);
 assert(iseq(Qu0'*Qu0,I,15))
 [vs,~,k] = vsigma(Qv,Qu');
 assert(iseq(Qv*Qu'*vs,Id(k),15))
+%%
+tic
+r = qr(a,2,@(A) null(A,'rational'));
+toc
--- R2026/boxdimsDemo.m
+++ R2026/boxdimsDemo.m
@@ -1,12 +1,16 @@
 %%
-% mex -R2018 -Drrlu spmex1.c
-% mex -R2018 -Dbsx spmex1.c
-mex -R2018 spmex1.c
-format compact
+% mex -R2018a -Drrlu spmex1.c
+% mex -R2018a -Dbsx spmex1.c
+mex -R2018a spmex1.c
 %%
 clear
+format compact
 a = boxdims(1,0.5,0.01);
 disp(a)
+%%
+r = qr(a,2,'Display','off');
+assert(degree(r) == degree(a)+2)
+disp(r)
 %%
 [Qu,r] = qr(a,2,'Display','iter');
 iseq = @(a,b,n) isequal( ...
@@ -14,13 +18,32 @@ iseq = @(a,b,n) isequal( ...
 [vs,~,k] = vsigma(Qu',a);
 assert(iseq(r(k),Qu'*a*vs,15))
 %%
+[Qv,Qu,r] = qr(a,2,'Display','off');
+Qr = simplify(Qv*r); % 'mindeg'
+assert(isequal(a,Qr(index(a))))
+[vs,~,k] = vsigma(Qu',a);
+assert(isequal(r(k),Qu'*a*vs))
+%%
+r = simplify(r);
+disp(r)
+assert(degree(r) == degree(a))
+%%
 k = index(r);
 x3 = roots(r(end,1,k));
-x = sort(x3,'descend');
+x = sort(x3,'descend') %#ok<NOPTS>
 polyval(a,x,1)
 %%
-clear
-a = boxdims(1,0.5,0.01);
+symx = sym('x',[3 1]);
+symr = polyval(r,symx,1);
+syma = polyval(a,symx,1);
+simplify(symr.'*diag([1 -2 100])- ...
+    gbasis(syma.','MonomialOrder','lex'))
+%%
+U = polyval(Qu',symx,1); % symU
+V = polyval(Qv,symx,1); % symV
+I = eye(3,3,'sym'); % symI
+assert(isequal(simplify(V*U),I))
+%%
 tic
 r = qr(a,2,@(A) null(A,'rational'));
 toc
